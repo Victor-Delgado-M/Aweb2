de9348a66c59c2f9044f1f85c85a1efaf7b1e6b8
Victor-Delgado-M <e1312078874@live.uleam.edu.ec>
2023-09-21 22:02:13 -0500

    cambio

--- PrimerParcial/practica1/Arrreglo.js
+++ PrimerParcial/practica1/Arrreglo.js
@@ -3,6 +3,7 @@
 
 const cevicherias= [
     {id:1, nombre:'Cevichería el TORO', direccion:"Barrio los Steros", ruc:"13117412451", slogan:"Lo mejor de lo mejor con nosotros",horario: "Lunes a Viernes desde 8:00 a 11:30 AM"},
+    
     {id:2, nombre:'Cevichería el MAR ', direccion:"Barrio la paz", ruc:"131174124321", slogan:"Lo mejor ",horario: "Lunes a Sabado desde 8:00 a 11:30 AM"},
     {id:3, nombre:'Cevichería el BLUE PARADISE', direccion:"Barrio la epoca", ruc:"1311743232451", slogan:"Aprovecha ahora un cevichazo",horario: "Lunes a Jueves desde 8:00 a 11:30 AM"},
     {id:4, nombre:'Cevichería el MI CAMARÓN', direccion:"Barrio Aldon ", ruc:"13134123250", slogan:"Animate a probar nuestro saber",horario: "Lunes a Miercoles desde 8:00 a 14:30 PM"},
